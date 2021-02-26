902b7229c36430455df6fab8cdb16906fd2561c5
Lucca Fraser <lucca.fraser@gmail.com>
2021-02-26 15:40:54 -0400

    adding x11 support for lxc

--- build-script.sh
+++ build-script.sh
@@ -3,32 +3,35 @@ err () {
   exit 1
 }
 
+[ -d "retro-venv" ] || virtualenv retro-venv
+source retro-venv/bin/activate
+
 trap err ERR
 
 echo "Installing dependencies available from distro repositories"
-apt update
-apt install -y build-essential cmake python3.8 python3.8-dev libbz2-dev pkg-config capnproto libcapnp-dev zlib1g-dev xvfb python-opengl python3-pip git
+sudo apt update
+sudo apt install -y build-essential cmake python3.8 python3.8-dev libbz2-dev pkg-config capnproto libcapnp-dev zlib1g-dev xvfb python-opengl python3-pip git
 
 echo "Installing dependencies with pip"
 pip3 install torch tensorflow numpy~=1.19.2
 
 echo "Cloning git repos"
-git clone https://github.com/oblivia-simplex/retro
+#git clone https://github.com/oblivia-simplex/retro
 git clone https://github.com/lejonet/baselines -b tf2
 
 echo "Installing lucca's fork of retrogym"
-pushd ~/retro/RetroGym
+pushd RetroGym
 pip3 install -e .
 python3 -m retro.import ./SMW
 popd
 
 echo "Installing a2c_ppo_acktr from MarioWM directory"
-pushd ~/retro/MarioWM
+pushd MarioWM
 pip3 install -e .
 popd
 
 echo "Installing baselines from own checkout"
-pushd ~/baselines
+pushd baselines
 pip3 install -e .
 popd
 
