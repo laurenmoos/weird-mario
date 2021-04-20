a0d71119ccface7ec90b95c9d5b2099796cf9372
Lauren Moos <laurenmoos@Laurens-MacBook-Air.local>
2021-04-20 13:03:04 -0700

    dockerizing project

--- MarioWM/prerequisite.sh
+++ MarioWM/prerequisite.sh
@@ -3,9 +3,7 @@
     exit 1
   }
   trap err ERR
-  echo "Installing dependencies available from distro repositories"
-  sudo apt update
-  sudo apt install -y build-essential cmake python3.8 python3.8-dev libbz2-dev pkg-config capnproto libcapnp-dev zlib1g-dev xvfb git curl python3-pip python3-opengl
+
   pushd /tmp
   echo "Downloading and installing anaconda"
   curl https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --output anaconda.sh
