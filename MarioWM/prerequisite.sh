7a392ea5ba547ab3fa6ed0e2b001bc2315fe3127
Lauren Moos <laurenmoos@Laurens-MacBook-Air.local>
2021-04-22 19:57:11 -0700

    update versions

--- MarioWM/prerequisite.sh
+++ MarioWM/prerequisite.sh
@@ -5,41 +5,44 @@
   trap err ERR
 
   pushd /tmp
-  echo "Downloading and installing anaconda"
-  curl https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --output anaconda.sh
-  chmod +x anaconda.sh
-  ./anaconda.sh -b
-  "${HOME}"/anaconda3/bin/conda init bash
-  popd
+#  echo "Downloading and installing anaconda"
+#  curl https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --output anaconda.sh
+#  chmod +x anaconda.sh
+#  ./anaconda.sh -b
+#  "${HOME}"/anaconda3/bin/conda init bash
+#  popd
   #!/usr/bin/env bash
-  err () {
-    exit 1
-  }
-  trap err ERR
-  __conda_setup="$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-  if [ $? -eq 0 ]; then
-      eval "$__conda_setup"
-  else
-      if [ -f "/root/anaconda3/etc/profile.d/conda.sh" ]; then
-          . "/root/anaconda3/etc/profile.d/conda.sh"
-      else
-          export PATH="/root/anaconda3/bin:$PATH"
-      fi
-  fi
-  unset __conda_setup
-  echo "Creating conda env 'retro-venv'"
-  conda create --name retro-venv --yes python=3.8
-  conda activate retro-venv
-  echo "Installing dependencies with conda"
-  conda install -y pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch
-  conda install -y tensorflow numpy~=1.19.2
+#  err () {
+#    exit 1
+#  }
+#  trap err ERR
+#  __conda_setup="$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#  if [ $? -eq 0 ]; then
+#      eval "$__conda_setup"
+#  else
+#      if [ -f "/root/anaconda3/etc/profile.d/conda.sh" ]; then
+#          . "/root/anaconda3/etc/profile.d/conda.sh"
+#      else
+#          export PATH="/root/anaconda3/bin:$PATH"
+#      fi
+#  fi
+#  unset __conda_setup
+#  echo "Creating conda env 'retro-venv'"
+#  conda create --name retro-venv --yes python=3.8
+#  conda activate retro-venv
+  echo "Installing dependencies with pip instead of conda "
+  pip3 install torch
+  pip3 install torchvision
+  pip3 install torchaudio
+  pip3 install cudatoolkit==11.0
+  pip3 install tensorflow
+  pip3 install numpy==1.19.2
   echo "Cloning git repos"
   git clone https://github.com/laurenmoos/weird-mario
   git clone https://github.com/lejonet/baselines -b tf2
   echo "Installing lucca's fork of retrogym"
   pushd weird-mario
   pushd RetroGym
-  pip3 install torch
   pip3 install -e .
   python3 -m retro.import ./SMW
   popd
