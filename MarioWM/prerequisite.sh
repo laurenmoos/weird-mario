460d2eaa4ad6d837e7924cb6a6356b6c7cf87eb1
Lauren Moos <laurenmoos@Laurens-MacBook-Air.local>
2021-04-22 21:11:39 -0700

    update dependencies

--- MarioWM/prerequisite.sh
+++ MarioWM/prerequisite.sh
@@ -4,37 +4,36 @@
   }
   trap err ERR
 
-  pushd /tmp
-#  echo "Downloading and installing anaconda"
-#  curl https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --output anaconda.sh
-#  chmod +x anaconda.sh
-#  ./anaconda.sh -b
-#  "${HOME}"/anaconda3/bin/conda init bash
-#  popd
-  #!/usr/bin/env bash
-#  err () {
-#    exit 1
-#  }
-#  trap err ERR
-#  __conda_setup="$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-#  if [ $? -eq 0 ]; then
-#      eval "$__conda_setup"
-#  else
-#      if [ -f "/root/anaconda3/etc/profile.d/conda.sh" ]; then
-#          . "/root/anaconda3/etc/profile.d/conda.sh"
-#      else
-#          export PATH="/root/anaconda3/bin:$PATH"
-#      fi
-#  fi
-#  unset __conda_setup
-#  echo "Creating conda env 'retro-venv'"
-#  conda create --name retro-venv --yes python=3.8
-#  conda activate retro-venv
+  echo "Downloading and installing anaconda"
+  curl https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --output anaconda.sh
+  chmod +x anaconda.sh
+  ./anaconda.sh -b
+  "${HOME}"/anaconda3/bin/conda init bash
+  popd
+  !/usr/bin/env bash
+  err () {
+    exit 1
+  }
+  trap err ERR
+  __conda_setup="$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+      eval "$__conda_setup"
+  else
+      if [ -f "/root/anaconda3/etc/profile.d/conda.sh" ]; then
+          . "/root/anaconda3/etc/profile.d/conda.sh"
+      else
+          export PATH="/root/anaconda3/bin:$PATH"
+      fi
+  fi
+  unset __conda_setup
+  echo "Creating conda env 'retro-venv'"
+  conda create --name retro-venv --yes python=3.8
+  conda activate retro-venv
   echo "Installing dependencies with pip instead of conda "
   pip3 install torch
   pip3 install torchvision
-  pip3 install torchaudio
-  pip3 install cudatoolkit==11.0
+  conda install torchaudio
+  conda install cudatoolkit==11.0
   pip3 install tensorflow
   pip3 install numpy==1.19.2
   echo "Cloning git repos"
@@ -54,4 +53,3 @@
   echo "Installing baselines from own checkout"
   pushd baselines
   pip3 install -e .
-  popd
