ecc008f9eefd40b22232fa684a141128692875bf
Lauren Moos <laurenmoos@Laurens-MacBook-Air.local>
2021-04-25 11:53:46 -0700

    added explicit pybullet install

--- MarioWM/prerequisite.sh
+++ MarioWM/prerequisite.sh
@@ -28,6 +28,7 @@
   conda activate retro-venv
   echo "Installing dependencies with conda "
   conda install -y pytorch cudatoolkit=11.0 -c pytorch
+  pip3 install pybullet 
   pip3 install tensorflow
   pip3 install numpy==1.19.2
   echo "Cloning git repos"
