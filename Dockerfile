7b5fa01fc7bd86295b0539d3263c555b25c750c8
Lauren Moos <laurenmoos@Laurens-MacBook-Air.local>
2021-04-25 19:10:58 -0700

    docker cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,23 @@
 FROM ubuntu:latest
+ARG USER
+ARG HOME
+
+RUN mkdir -p $HOME
+WORKDIR $HOME
+ENV LANG=C.UTF-8 LC_ALL=C.UTF-8 USER=$USER HOME=$HOME
+
+RUN echo "The working directory is: $HOME"
+RUN echo "The user is: $USER"
 
 RUN apt-get update && apt-get -y install sudo
 RUN adduser --disabled-password --gecos '' docker
 RUN adduser docker sudo
 RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
-USER docker
-#TODO: this will persist after build and is not a best practice
-#COPY prerequisite.sh /tmp/prerequiste.sh
-#CMD /tmp/prerequiste.sh
+COPY ./MarioWM/ /
+
+COPY ./RetroGym/ /
+
 RUN echo "Installing dependencies available from distro repositories"
 #install tzdata package
 RUN sudo apt -y install -y tzdata
@@ -17,13 +26,18 @@ RUN sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 RUN sudo dpkg-reconfigure --frontend noninteractive tzdata
 RUN sudo apt-get -y install -y build-essential cmake python3.8 python3.8-dev libbz2-dev pkg-config capnproto libcapnp-dev zlib1g-dev xvfb git curl python3-pip python3-opengl
 
-
-COPY prerequisite.sh .
 RUN sudo ./prerequisite.sh
-RUN echo 'directory after run'
-RUN pwd
 
-#COPY /weird-mario/MarioWM $HOME/MarioWM
+RUN echo "Cloning Baselines"
+WORKDIR $HOME
+RUN git clone https://github.com/lejonet/baselines -b tf2
+RUN pip3 install -e .
+
+WORKDIR $HOME/RetroGym
+RUN echo "Installing lucca's fork of retrogym"
+RUN pip3 install -e .
+RUN python3 -m retro.import ./SMW
+
 WORKDIR $HOME/MarioWM
-#CMD ["MarioWM/main.py", "--exp-name", "--device"]
-#ENTRYPOINT ["python3"]
+CMD ["main.py", "--exp-name", "--device"]
+ENTRYPOINT ["python3"]
--- MarioWM/prerequisite.sh
+++ MarioWM/prerequisite.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
-  err () {
-    exit 1
-  }
-  trap err ERR
+#  err () {
+#    exit 1
+#  }
+#  trap err ERR
+#
+#  echo "Downloading and installing anaconda"
+#  curl https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --output anaconda.sh
+#  chmod +x anaconda.sh
+#  ./anaconda.sh -b
+#  "${HOME}"/anaconda3/bin/conda init bash
+#  # {/tmp}
+#
+#  __conda_setup="$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#  if [ $? -eq 0 ]; then
+#      eval "$__conda_setup"
+#  else
+#      if [ -f "/root/anaconda3/etc/profile.d/conda.sh" ]; then
+#          . "/root/anaconda3/etc/profile.d/conda.sh"
+#      else
+#          export PATH="/root/anaconda3/bin:$PATH"
+#      fi
+#  fi
+#  unset __conda_setup
+  RUN echo 'export PATH=/opt/conda/bin:$PATH' > /etc/profile.d/conda.sh \
+    && wget --quiet https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O ~/anaconda.sh \
+    && /bin/bash ~/anaconda.sh -b -p /opt/conda \
+    && rm ~/anaconda.sh
 
-  echo "Downloading and installing anaconda"
-  curl https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --output anaconda.sh
-  chmod +x anaconda.sh
-  ./anaconda.sh -b
-  "${HOME}"/anaconda3/bin/conda init bash
-  # {/tmp}
-
-  __conda_setup="$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-  if [ $? -eq 0 ]; then
-      eval "$__conda_setup"
-  else
-      if [ -f "/root/anaconda3/etc/profile.d/conda.sh" ]; then
-          . "/root/anaconda3/etc/profile.d/conda.sh"
-      else
-          export PATH="/root/anaconda3/bin:$PATH"
-      fi
-  fi
-  unset __conda_setup
   echo "Creating conda env 'retro-venv'"
   conda create --name retro-venv --yes python=3.8
   conda activate retro-venv
@@ -30,14 +35,4 @@
   pip3 install pybullet
   pip3 install tensorflow
   pip3 install numpy==1.19.2
-  echo "Cloning git repos"
-  git clone https://github.com/lejonet/baselines -b tf2
-  echo "Installing lucca's fork of retrogym"
-  pushd RetroGym
-  pip3 install -e .
-  python3 -m retro.import ./SMW
-  popd
-  echo "Installing baselines from own checkout"
-  pushd baselines
-  pip3 install -e .
-  popd
+
