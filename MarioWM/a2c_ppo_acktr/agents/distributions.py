8915ab195810810546867dcd9617537658d8984a
Lauren Moos <laurenmoos@Laurens-MacBook-Air.local>
2021-04-30 12:53:21 -0700

    added diag gaussian

--- MarioWM/a2c_ppo_acktr/agents/distributions.py
+++ MarioWM/a2c_ppo_acktr/agents/distributions.py
@@ -57,7 +57,27 @@ class Categorical(nn.Module):
         x = self.linear(x)
         return FixedCategorical(logits=x)
 
+class DiagGaussian(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(DiagGaussian, self).__init__()
 
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                               constant_(x, 0))
+
+        self.fc_mean = init_(nn.Linear(num_inputs, num_outputs))
+        self.logstd = AddBias(torch.zeros(num_outputs))
+
+    def forward(self, x):
+        action_mean = self.fc_mean(x)
+
+        #  An ugly hack for my KFAC implementation.
+        zeros = torch.zeros(action_mean.size())
+        if x.is_cuda:
+            zeros = zeros.cuda()
+
+        action_logstd = self.logstd(zeros)
+        return FixedNormal(action_mean, action_logstd.exp())
+    
 class FixedDiagGaussian(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(FixedDiagGaussian, self).__init__()
